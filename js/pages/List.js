3c5d16b2ad4b9ffceb05afa07adbd0ba643aa2d4
GoostYT <dc.snepvangers@gmail.com>
2026-08-26 19:34:57 +0200

    Fix punctuation and clarify Legacy List rules

--- js/pages/List.js
+++ js/pages/List.js
@@ -115,13 +115,13 @@ export default {
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Do not use secret routes or bug routes.
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Do not use easy modes, only a record of the unmodified level qualifies.
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, we still accept records for the level.
                     </p>
                 </div>
             </div>
